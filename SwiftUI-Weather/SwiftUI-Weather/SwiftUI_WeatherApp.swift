55983fc029dd0f760a03ee063fa658e5b9011f92
Juan Antonio Carrasco del Cid <juan.carrascodelcid@plexus.com>
2024-02-22 09:17:27 +0100

    First commit

---
+++ SwiftUI-Weather/SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Juan Antonio Carrasco del Cid on 22/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
